b7b6f87b1e6593dae790a81385e7247e23e90970
Ihar <mauler1987@gmail.com>
2020-03-13 13:36:36 +0300

    feat: add images mixer and form reaction

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const MENU = document.getElementById('menu');
 const BUTTONS = document.getElementById('porfolio-buttons');
 const IMAGES = document.getElementById('examples')
+var visible = true;
 
 MENU.addEventListener('click', (event) => {
   MENU.querySelectorAll('a').forEach(el => el.classList.remove('active'));
@@ -18,24 +19,55 @@ IMAGES.addEventListener('click', (event) => {
 })
 
 //power off for phones
-var visible = true;
 
 function showFun1() {
-  if(visible) {
-      document.getElementById('display-1').style.visibility = 'hidden';
-      visible = false;
-  } else {
-      document.getElementById('display-1').style.visibility = 'visible';
-      visible = true;
+      if(visible) {
+          document.getElementById('display-1').style.visibility = 'hidden';
+          visible = false;
+      } else {
+          document.getElementById('display-1').style.visibility = 'visible';
+          visible = true;
+      }
   }
+
+  function showFun2() {
+    if(visible) {
+        document.getElementById('display-2').style.visibility = 'hidden';
+        visible = false;
+    } else {
+        document.getElementById('display-2').style.visibility = 'visible';
+        visible = true;
+    }
 }
 
-function showFun2() {
-if(visible) {
-    document.getElementById('display-2').style.visibility = 'hidden';
-    visible = false;
-} else {
-    document.getElementById('display-2').style.visibility = 'visible';
-    visible = true;
-}
-}
+//form
+
+document.querySelector('.submit-form').addEventListener('click', (evt) => {
+  evt.preventDefault();
+  let NAME = document.querySelector('#input-1-area').value;
+  NAME = NAME === '' ? 'No name' : 'Name: ' + NAME;
+  let subject = document.querySelector('#subject').value;
+  subject = subject === '' ? 'No subject' : 'Subject: ' + subject;
+  let description = document.querySelector('#describe-area').value;
+  description = description === '' ? 'Empty' : 'Description: ' + description;
+  let message = 'Email sent \n'+NAME+'\n'+subject+'\n'+description;
+  alert(message);
+})
+
+//sort picture
+
+document.querySelectorAll('.portfolio-navigation').forEach( (item)=>{
+  item.addEventListener('click', (evt)=>{
+    let arr = [0,1,2,3,4,5,6,7,8,9,10,11];
+    let arrRandom = [];
+    for (let i = 0; i < 12; i++) {
+      let numRandom = Math.ceil(Math.random() * arr.length-1);
+      arrRandom.push( arr.splice(numRandom, 1) );
+    }
+    let i = 0;
+    document.querySelectorAll('.project-images').forEach( (item) => {
+      item.style.order = arrRandom[i];
+      i++;
+    })
+  })
+})
