3fc4e607f5c43a47110a290efc9c5132e1139d88
xcore33 <mauler1987@gmail.com>
2020-03-14 19:22:44 +0300

    feat: carousel type slider

--- index.js
+++ index.js
@@ -21,23 +21,23 @@ IMAGES.addEventListener('click', (event) => {
 //power off for phones
 
 function showFun1() {
-      if(visible) {
-          document.getElementById('display-1').style.visibility = 'hidden';
-          visible = false;
-      } else {
-          document.getElementById('display-1').style.visibility = 'visible';
-          visible = true;
-      }
+  if(visible) {
+    document.getElementById('display-1').style.visibility = 'hidden';
+    visible = false;
+  } else {
+    document.getElementById('display-1').style.visibility = 'visible';
+    visible = true;
   }
+}
 
-  function showFun2() {
-    if(visible) {
-        document.getElementById('display-2').style.visibility = 'hidden';
-        visible = false;
-    } else {
-        document.getElementById('display-2').style.visibility = 'visible';
-        visible = true;
-    }
+function showFun2() {
+  if(visible) {
+    document.getElementById('display-2').style.visibility = 'hidden';
+    visible = false;
+  } else {
+    document.getElementById('display-2').style.visibility = 'visible';
+    visible = true;
+  }
 }
 
 //form
@@ -71,3 +71,75 @@ document.querySelectorAll('.portfolio-navigation').forEach( (item)=>{
     })
   })
 })
+
+//slider
+
+const SLIDER = document.getElementById('phones');
+let slides = document.querySelectorAll('.slider-visible-area');
+let actualSlide = 0;
+let carouselAxis = false;
+
+function carousel() {
+  let position = 0;
+  let nextSlide = (actualSlide === 0) ? 1 : 0;
+  SLIDER.innerHTML = '';
+  let elem = slides[nextSlide].cloneNode(true);
+  elem.style.left = position*1000 - 1000 + 'px';
+  slides[actualSlide].style.left = position*1000 + 'px';
+  position += 1;
+  slides[nextSlide].style.left = position*1000 + 'px';
+  SLIDER.appendChild(elem);
+  SLIDER.appendChild(slides[actualSlide]);
+  SLIDER.appendChild(slides[nextSlide]);
+}
+
+function pushLeft() {
+  if (!carouselAxis) {
+    carouselAxis = true;
+    let slides2 = document.querySelectorAll('.slider-visible-area');
+    let position2 = -1;
+    for (let i = 0; i < slides2.length; i += 1) {
+      slides2[i].style.left = position2*1000 - 1000 +'px';
+      position2 += 1;
+    }
+    actualSlide += 1;
+    if (actualSlide >= slides.length) {
+      actualSlide = 0;
+    }
+    if (actualSlide === 1) {
+      document.getElementById('slider-picture-box').classList.add('trinity-background');
+    } else {
+      document.getElementById('slider-picture-box').classList.remove('trinity-background');
+    }
+  }
+}
+
+function pushRight() {
+  if (!carouselAxis) {
+    carouselAxis = true;
+    let slides2 = document.querySelectorAll('.slider-visible-area');
+    let position2 = -1;
+    for (let i = 0; i < slides2.length; i += 1) {
+      slides2[i].style.left = position2*1000 + 1000 +'px';
+      position2 += 1;
+    }
+    actualSlide += 1;
+    if (actualSlide >= slides.length) {
+      actualSlide = 0;
+    }
+    if (actualSlide === 1) {
+      document.getElementById('slider-picture-box').classList.add('trinity-background');
+    } else {
+      document.getElementById('slider-picture-box').classList.remove('trinity-background');
+    }
+  }
+}
+
+SLIDER.addEventListener('transitionend', function () {
+  carousel();
+  carouselAxis = false;
+});
+
+document.getElementById('left-button').addEventListener('click', pushLeft);
+document.getElementById('right-button').addEventListener('click', pushRight);
+carousel();
